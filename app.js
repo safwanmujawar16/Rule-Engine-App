0c9c63debd719f75c149fb4c6147e729d1d4f710
Safwan_Mujawar <139694210+safwanmujawar16@users.noreply.github.com>
2024-10-26 00:28:32 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
