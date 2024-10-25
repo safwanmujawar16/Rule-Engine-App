fa68317d65a3f6b3dc280c1071fc73f5d92a3342
Safwan_Mujawar <139694210+safwanmujawar16@users.noreply.github.com>
2024-10-26 00:09:58 +0530

    Updated db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://0.0.0.0:27017/rule-engine');  // No deprecated options
+        await mongoose.connect('mongodb://localhost:27017/rule-engine');  // No deprecated options
         console.log('MongoDB connected...');
     } catch (err) {
         console.error(err.message);
