a3409c890e99969e4ee5fec4608e68f10def3f32
Safwan_Mujawar <139694210+safwanmujawar16@users.noreply.github.com>
2024-10-25 23:54:30 +0530

    Delete badckend directory

--- backend/models/ruleModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const RuleSchema = new mongoose.Schema({
-    ruleString: { type: String, required: true },
-    ast: { type: Object, required: true },  // Store the AST as a JSON object
-});
-
-module.exports = mongoose.model('Rule', RuleSchema);
--- backend/routes/ruleRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createRule, combineRules, evaluateRule } = require('../controllers/ruleController');
-const router = express.Router();
-
-router.post('/create', createRule);
-router.post('/combine', combineRules);
-router.post('/evaluate', evaluateRule);
-
-module.exports = router;
