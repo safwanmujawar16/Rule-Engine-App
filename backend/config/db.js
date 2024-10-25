5bd4b4d3a969ed57c647b558b10b5b11cf17adb2
Safwan_Mujawar <139694210+safwanmujawar16@users.noreply.github.com>
2024-10-26 01:11:19 +0530

    Updated db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/rule-engine');  // No deprecated options
+        await mongoose.connect('mongodb+srv://safwanmujawar16:mypassword@rule-engine.rqip4.mongodb.net/?retryWrites=true&w=majority&appName=rule-engine');  // No deprecated options
         console.log('MongoDB connected...');
     } catch (err) {
         console.error(err.message);
